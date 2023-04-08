f8173d3f843a84821d662d3e900268813c3501cb
caiordm <caiordm08@gmail.com>
2023-04-08 14:33:40 -0300

    Home Screen criada

--- home.php
+++ home.php
@@ -22,10 +22,30 @@ while ($row = fgetcsv($dataUsers)) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
+    <link rel="stylesheet" href="styleHome.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Big+Shoulders+Display:wght@400;600;800&family=Bebas+Neue&display=swap&family=Open+Sans:wght@500&display=swap" rel="stylesheet">
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
 </head>
 
 <body>
-    <h1>Voce entrou! <?= $name ?></h1>
+    <div class="menu">dfvrtgtrf</div>
+    <div class="content">
+        <section class="section1">
+            <section class="section2">
+                <h1>Olá, <?= strtoupper($name) ?>!</h1>
+            </section>
+            <section class="section3">
+                <button id="my-button" class="newHistory">
+                    <ion-icon id="add-circle" name="add-circle"></ion-icon>
+                    <span>New history</span>
+                </button>
+            </section>
+        </section>
+    </div>
+    <script src="historys.js"></script>
 </body>
 
 </html>
