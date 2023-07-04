fe006fcfe5b883f390be2450df1350d4b9e22f87
caiordm <caiordm08@gmail.com>
2023-07-04 10:37:53 -0300

    fix: erros de sessão

--- controller/loginUsuario.php
+++ controller/loginUsuario.php
@@ -1,16 +1,15 @@
 <?php 
 
 require_once('../model/conexao.php');
-session_start();
 
 try {
     if (!isset($_POST['email']) && !isset($_POST['password'])) {
         die('Dados não chegaram corretamente!');
     }
-
+    
     $email = $_POST['email'];
     $password = $_POST['password'];
-
+    
     $query = "SELECT * FROM users WHERE email = :email AND password = :password";
     $stmt = $conn->prepare($query);
 
@@ -18,15 +17,15 @@ try {
     $stmt->bindParam(':password', $password);
     $stmt->execute();
 
-    $users = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
+    $users = $stmt->fetch(PDO::FETCH_ASSOC);
+    
     if($users) {
-        var_dump($users);
+        session_start();
         $_SESSION['auth'] = true;
         $_SESSION['name'] = $users["name"];
         $_SESSION['email'] = $users["email"];
         $_SESSION['password'] = $users['password'];
-        header('location: ../view/home');
+        header('location: ../view/homepage');
         exit();
     } else {
         header('location: ../view/login/index.php?err=Email ou senha errados');
---
+++ view/homepage/index.php
@@ -0,0 +1,27 @@
+<?php
+
+session_start();
+$name = $_SESSION["name"];
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet" href="../public/css/styleHome.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Big+Shoulders+Display:wght@400;600;800&family=Open+Sans:wght@500&display=swap" rel="stylesheet">
+</head>
+<body>
+    <aside>
+        <p>r233333de3</p>
+    </aside>
+    <main>
+
+    </main>
+</body>
+</html>
